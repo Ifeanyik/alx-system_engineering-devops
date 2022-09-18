44b505ac2f05d6de15a972fbbe20d144d7a223f9
Ifeanyik <jax64720@gmail.com>
2022-09-18 10:27:04 -0700

    Final effort

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,7 @@
 # this script configures the client
 # to authenticate without a password
-
-$string = 'Host 3.229.142.17
-  PasswordAuthentication no
-  IdentityFile ~/.ssh/school'
-
-file { '/.ssh/config':
-  ensure  => absent,
-  content => $string,
-  path => '/.ssh/config'
+exec { 'echo':
+  path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
 }
